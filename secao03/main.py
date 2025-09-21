a65324d1f94523f661bb2235d36c6a1d42e51dc5
Giancarlo Haack Albano <gian418@gmail.com>
2025-09-21 16:20:52 -0300

    secao03

--- secao03/main.py
+++ secao03/main.py
@@ -1,6 +1,11 @@
+from typing import List, Optional
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
+from fastapi import Response
+from fastapi import Path
+from fastapi import Query
+from models import Curso
 
 app = FastAPI()
 
@@ -23,13 +28,42 @@ async def get_cursos():
     return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3)):
     try:
         curso = cursos[curso_id]
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado.")
     
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
+    return curso
+
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com o id {curso_id}")
+    
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado.")
+    
+@app.get('/calculadora')
+async def calculadora(a: int = Query(gt=5), b: int = Query(gt=10), c: Optional[int] = 0):
+    soma = a + b + c
+    return {"resultado": soma}
+
 
 
 if __name__ == '__main__':
